03880dd9a5a1f19c6601a09252aca9427edd443e
le.zhang <le.zhang@silot.ai>
2022-07-23 14:36:10 +0800

    update the package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="ml_utils",
+    name="ml_performance_utils",
     version="0.0.1",
     packages=find_packages(),
     install_requires=[],
